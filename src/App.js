a206cdff695622e7675c749b8799aa7f2f23b445
Artur Brytkowski <96352398+allala0@users.noreply.github.com>
2022-11-29 04:07:11 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, {useEffect, useRef} from "react";
 import * as THREE from 'three';
-import ThreeEngine from './lib/three-engine/ThreeEngine';
+import ThreeEngine from 'three-engine';
+import Transition from 'three-engine/tools/transitions/Transition';
+import TransitionObject from 'three-engine/tools/transitions/TransitionObject';
+import transitionFunctions from 'three-engine/tools/transitions/transitionFunctions';
 
 function App(){
     let threeEngine;
+    let transition = null;
 
     const ref = useRef(null);
     
@@ -22,14 +26,23 @@ function App(){
                 initLoaded: threeEngine => {
                     const box = new THREE.Mesh(new THREE.BoxGeometry(0.2, 0.2, 0.2), new THREE.MeshStandardMaterial({color: 0xff0000}));
                     box.setupClickable();
+                    
+                    box.onClick = () => transition = new Transition([
+                        new TransitionObject(box, 'scale', {0: new THREE.Vector3(1, 1, 1), 0.5: new THREE.Vector3(1.5, 1.5, 1.5), 1: new THREE.Vector3(1, 1, 1)}),
+                        new TransitionObject(box, 'position', {0: new THREE.Vector3(), 0.5: new THREE.Vector3(0, 0.2, 0), 1: new THREE.Vector3()}),
+                        new TransitionObject(box.rotation, 'y', {0: 0, 0.5: Math.PI / 2, 1: -Math.PI}),
+                    ], {autoplay: true, function: transitionFunctions.easeInOutCubic, duration: 1});
                     threeEngine.scene.add(box);
+                },
+                update: threeEngine => {
+                    if(transition) transition.update();
                 }
             }
         });
     }, []);
 
     return (
-        <div style={{height: '1000px'}} ref={ref}></div>
+        <div className='scene' ref={ref}></div>
     );
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,10 @@ module.exports = (env, argv) => {
                     use: {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['@babel/preset-env', '@babel/preset-react']
+                            presets: [
+                                '@babel/preset-env', 
+                                '@babel/preset-react'
+                            ]
                         }
                     }
                 },
